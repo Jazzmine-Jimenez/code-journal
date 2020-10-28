4d975d5480cda6efc513a5e40e179e9817fa0f09
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-10-27 17:28:27 -0700

    Define a JavaScript function that takes the profile data model and returns a DOM tree that matches the HTML.#2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// ------------------------EDITING PROFILE
 var $avatarURL = document.getElementById('avatar-URL');
 var $avatarImage = document.getElementById('avatarImage');
 var $form = document.querySelector('form');
@@ -30,3 +31,63 @@ window.addEventListener('beforeunload', function (event) {
   var savedDataJson = JSON.stringify(data);
   localStorage.setItem('data', savedDataJson);
 });
+
+// ----------------VIEWING PROFILE
+function domTreeCreation(model) {
+
+  var $profile = document.createElement('div');
+  $profile.setAttribute('data-view', 'profile');
+
+  var $container = document.createElement('div');
+  $container.setAttribute('class', 'container');
+  $profile.appendChild($container);
+
+  // First Row ------------------------------------
+  var $row1 = document.createElement('div');
+  $row1.setAttribute('class', 'row');
+  $container.appendChild($row1);
+
+  var $columnFull = document.createElement('div');
+  $columnFull.setAttribute('class', 'column-full');
+  $row1.appendChild($columnFull);
+
+  var $headerName = document.createElement('h1');
+  var $name = document.createTextNode(model.fullName);
+  $headerName.appendChild($name);
+  $columnFull.appendChild($headerName);
+
+  // Second Row ----------------------------------------
+  var $row2 = document.createElement('div');
+  $row2.setAttribute('class', 'row');
+  $container.appendChild($row2);
+
+  var $columnHalf1 = document.createElement('div');
+  $columnHalf1.setAttribute('class', 'column-half');
+  $row2.appendChild($columnHalf1);
+
+  var $profilePic = document.createElement('img');
+  $profilePic.setAttribute('class', 'avatarImage');
+  $profilePic.setAttribute('src', model.avatarUrl);
+  $columnHalf1.appendChild($profilePic);
+
+  var $columnHalf2 = document.createElement('div');
+  $columnHalf2.setAttribute('class', 'column-half');
+  $row2.appendChild($columnHalf2);
+
+  var $username = document.createElement('span');
+  $username.setAttribute('class', 'far fa-user');
+  var $addingUsername = document.createTextNode(model.username);
+  $username.appendChild($addingUsername);
+  $columnHalf2.appendChild($username);
+
+  var $location = document.createElement('span');
+  $location.setAttribute('class', 'far fa-map-marker-alt');
+  var $addingLocation = document.createTextNode(model.location);
+  $location.appendChild($addingLocation);
+  $columnHalf2.appendChild($location);
+
+  var $bio = document.createElement('p');
+  var $addingBio = document.createTextNode(model.bio);
+  $bio.appendChild($addingBio);
+  $columnHalf2.appendChild($bio);
+}
